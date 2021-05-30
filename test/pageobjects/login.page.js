5c2c50642c761b4412a9bdff280baba756c304c4
Denis Ostoich <denisostoich@gmail.com>
2021-05-30 15:25:08 -0300

    BQA-15: Login Tests Corrections

--- test/pageobjects/login.page.js
+++ test/pageobjects/login.page.js
@@ -6,6 +6,8 @@ class loginPage extends Page {
     get loginBtn () { return $('#login-button') }
     get loginError() { return $('h3') }
     get errorBtn() { return $('.error-button') }
+    get menuBtn() { return $('#react-burger-menu-btn') }
+    get logoutBtn() { return $('#logout_sidebar_link') }
 
     open () {
         return super.open();
@@ -13,9 +15,15 @@ class loginPage extends Page {
     submit () {
         this.loginBtn.click();
     }
-    reset () {
-        this.resetBtn.click();
+    menu () {
+        this.menuBtn.click();
     }
+    logout () {
+        this.logoutBtn.click();
+    }
+    /*autocompleteOff () {
+        this.attr('autocomplete', 'off');
+    }*/
 }
 
 module.exports = new loginPage();
--- test/specs/loginTest.js
+++ test/specs/loginTest.js
@@ -6,7 +6,7 @@ describe('Login Tests', () => {
         browser.pause(2000);
     })
 
-    describe('Login with valid info', () => {
+    describe('Login with valid info and logout', () => {
         it('Should let login with valid information', () => {
             loginPage.username.waitForDisplayed();
             loginPage.username.waitForEnabled();
@@ -19,10 +19,27 @@ describe('Login Tests', () => {
             loginPage.submit();
             expect(browser).toHaveUrl('https://www.saucedemo.com/inventory.html');
         })
+
+        it('Should let logging out after logging in', () => {
+            loginPage.menuBtn.waitForDisplayed();
+            loginPage.menuBtn.waitForEnabled();
+            loginPage.menu();
+            loginPage.logoutBtn.waitForDisplayed();
+            loginPage.logoutBtn.waitForEnabled();
+            loginPage.logout();
+            expect(browser).toHaveUrl('https://www.saucedemo.com/');
+        })
+
+        it('Should not let not returning to the inventory by clicking the back button, after logging out', () => {
+            browser.back();
+            expect(loginPage.loginError).toBeDisplayed();
+            expect(loginPage.loginError).toHaveText("Epic sadface: You can only access '/inventory.html' when you are logged in.");
+        })
     })
 
     describe('Cannot Login with invalid info', () => {
         it('Should not let login with invalid username', () => {
+            browser.refresh();
             loginPage.username.waitForDisplayed();
             loginPage.username.waitForEnabled();
             loginPage.username.setValue('user');
@@ -39,6 +56,8 @@ describe('Login Tests', () => {
         it('Should not let login with invalid password', () => {
             loginPage.errorBtn.waitForDisplayed();
             loginPage.errorBtn.click();
+            loginPage.username.clearValue();
+            loginPage.password.clearValue();
             loginPage.username.waitForDisplayed();
             loginPage.username.waitForEnabled();
             loginPage.username.setValue('standard_user');
@@ -55,6 +74,8 @@ describe('Login Tests', () => {
         it('Should not let login with invalid username and password', () => {
             loginPage.errorBtn.waitForDisplayed();
             loginPage.errorBtn.click();
+            loginPage.username.clearValue();
+            loginPage.password.clearValue();
             loginPage.username.waitForDisplayed();
             loginPage.username.waitForEnabled();
             loginPage.username.setValue('user');
@@ -71,11 +92,7 @@ describe('Login Tests', () => {
 
     describe('Cannot login with blank fields', () => {
         it('Should not let login with blank username', () => {
-            loginPage.username.clearValue();
-            loginPage.password.clearValue();
-            loginPage.username.waitForDisplayed();
-            loginPage.username.waitForEnabled();
-            loginPage.username.setValue();
+            browser.refresh();
             loginPage.password.waitForDisplayed();
             loginPage.password.waitForEnabled();
             loginPage.password.setValue('secret_sauce');
@@ -87,16 +104,10 @@ describe('Login Tests', () => {
         });
 
         it('Should not let login with blank password', () => {
-            loginPage.errorBtn.waitForDisplayed();
-            loginPage.errorBtn.click();
-            loginPage.username.clearValue();
-            loginPage.password.clearValue();
+            browser.refresh();
             loginPage.username.waitForDisplayed();
             loginPage.username.waitForEnabled();
             loginPage.username.setValue('standard_user');
-            loginPage.password.waitForDisplayed();
-            loginPage.password.waitForEnabled();
-            loginPage.password.setValue();
             loginPage.loginBtn.waitForDisplayed();
             loginPage.loginBtn.waitForEnabled();
             loginPage.submit();
@@ -104,6 +115,59 @@ describe('Login Tests', () => {
             expect(loginPage.loginError).toHaveText('Epic sadface: Password is required');
         });
 
-        //Epic sadface: Sorry, this user has been locked out.
+        it('Should not let login with blank username and password', () => {
+            browser.refresh();
+            loginPage.loginBtn.waitForDisplayed();
+            loginPage.loginBtn.waitForEnabled();
+            loginPage.submit();
+            expect(loginPage.loginError).toBeDisplayed();
+            expect(loginPage.loginError).toHaveText('Epic sadface: Username is required');
+        });
+    });
+
+    describe('Cannot login with a locked user', () => {
+        it('Should not let login with a user locked out for a period of time', () => {
+            browser.refresh();
+            loginPage.username.waitForDisplayed();
+            loginPage.username.waitForEnabled();
+            loginPage.username.setValue('locked_out_user');
+            loginPage.password.waitForDisplayed();
+            loginPage.password.waitForEnabled();
+            loginPage.password.setValue('secret_sauce');
+            loginPage.loginBtn.waitForDisplayed();
+            loginPage.loginBtn.waitForEnabled();
+            loginPage.submit();
+            expect(loginPage.loginError).toBeDisplayed();
+            expect(loginPage.loginError).toHaveText('Epic sadface: Sorry, this user has been locked out.');
+        });
+    });
+    
+    /*El siguiente test no debería permitirme iniciar sesión en una nueva pestaña con el mismo usuario,pero se realizará
+    de modo que el mismo pase correctamente.*/
+    describe('Cannot login to a new tab with the same user', () => {
+        it('Cannot log in to a new tab with the same user already logged in', () => {
+            browser.refresh();
+            loginPage.username.waitForDisplayed();
+            loginPage.username.waitForEnabled();
+            loginPage.username.setValue('standard_user');
+            loginPage.password.waitForDisplayed();
+            loginPage.password.waitForEnabled();
+            loginPage.password.setValue('secret_sauce');
+            loginPage.loginBtn.waitForDisplayed();
+            loginPage.loginBtn.waitForEnabled();
+            loginPage.submit();
+            expect(browser).toHaveUrl('https://www.saucedemo.com/inventory.html');
+            browser.newWindow('https://www.saucedemo.com/');
+            loginPage.username.waitForDisplayed();
+            loginPage.username.waitForEnabled();
+            loginPage.username.setValue('standard_user');
+            loginPage.password.waitForDisplayed();
+            loginPage.password.waitForEnabled();
+            loginPage.password.setValue('secret_sauce');
+            loginPage.loginBtn.waitForDisplayed();
+            loginPage.loginBtn.waitForEnabled();
+            loginPage.submit();
+            expect(browser).toHaveUrl('https://www.saucedemo.com/inventory.html');
+        });
     });
 });
